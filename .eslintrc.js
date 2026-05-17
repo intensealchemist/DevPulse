33ba0748032d2084d7e41d81c4bbc83a171254cf
intensealchemist <162778488+intensealchemist@users.noreply.github.com>
2026-05-17 21:59:23 +0530

    ci: fix ESLint warnings and Node20 Github Actions deprecation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/no-unstable-nested-components': [
+      'warn',
+      { allowAsProps: true },
+    ],
+  },
 };
